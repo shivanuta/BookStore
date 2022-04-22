c96544dc42f66e81ce4413ff76a0fd6b7abcfd10
shivanuta <nutakki.shivaramakrishna99@gmail.com>
2022-04-22 12:59:45 +0530

    user login test cases added

--- BookStore/BookStore-Test/Controllers/UsersControllerTest.cs
+++ BookStore/BookStore-Test/Controllers/UsersControllerTest.cs
@@ -92,5 +92,87 @@ namespace BookStore_Test.Controllers
             var okResult = result as OkObjectResult;
             Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
         }
+
+        [Fact]
+        public void UserController_UserLoginSuccess_Test()
+        {
+            var mockUserService = new Mock<IUserService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var controller = new UsersController(mockUserService.Object, mockMapper.Object);
+
+            var authenticateRequest = new AuthenticateRequest
+            {
+                Username = "shiva",
+                Password = "123123"
+            };
+            var result = controller.Authenticate(authenticateRequest);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+        }
+
+        [Fact]
+        public void UserController_UserLogin_MissingUserneme_Test()
+        {
+            var mockUserService = new Mock<IUserService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var controller = new UsersController(mockUserService.Object, mockMapper.Object);
+
+            var authenticateRequest = new AuthenticateRequest
+            {
+                Password = "123123"
+            };
+            var result = controller.Authenticate(authenticateRequest);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+        }
+        [Fact]
+        public void UserController_UserLogin_MissingPassword_Test()
+        {
+            var mockUserService = new Mock<IUserService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var controller = new UsersController(mockUserService.Object, mockMapper.Object);
+
+            var authenticateRequest = new AuthenticateRequest
+            {
+                Username = "shiva",
+            };
+            var result = controller.Authenticate(authenticateRequest);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+        }
+        [Fact]
+        public void UserController_UserLogin_EmptyUsernamePassword_Test()
+        {
+            var mockUserService = new Mock<IUserService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var controller = new UsersController(mockUserService.Object, mockMapper.Object);
+
+            var authenticateRequest = new AuthenticateRequest();
+
+            var result = controller.Authenticate(authenticateRequest);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+        }
+        [Fact]
+        public void UserController_UserLogin_InvalidCredentials_Test()
+        {
+            var mockUserService = new Mock<IUserService>();
+            var mockMapper = new Mock<IMapper>();
+
+            var controller = new UsersController(mockUserService.Object, mockMapper.Object);
+
+            var authenticateRequest = new AuthenticateRequest
+            {
+                Username = "test",
+                Password = "test"
+            };
+            var result = controller.Authenticate(authenticateRequest);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+        }
     }
 }
